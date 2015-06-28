ec8a5c3790ffd4e9a0bde52ba338d4965fd7e6d9
chucshary <shary-princess@hotmail.com>
2015-06-28 01:41:38 -0500

    layouts

---
+++ app/src/main/java/shary/recetas/activity/FavoritosFragment.java
@@ -0,0 +1,46 @@
+package shary.recetas.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import shary.recetas.R;
+
+/**
+ * Created by Shary on 27/06/2015.
+ */
+public class FavoritosFragment extends Fragment {
+    public FavoritosFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_favoritos, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
